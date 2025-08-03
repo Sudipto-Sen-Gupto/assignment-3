0b20a64c1b2c1e3179a4269d70029a68a5d1c333
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-03 15:11:31 -0500

    problem five added

---
+++ problem4.js
@@ -0,0 +1,11 @@
+/** Problem 04 - (Delete / Store) */
+var fileName= "#exp.mp4";
+//write your code here
+
+if(fileName.includes('#')|| fileName.includes(".pdf")|| fileName.includes(".docs")){
+  console.log("store");
+}
+else{
+    console.log("delete");
+}
+// fileName=fileName.slice(1).includes('#');
