76a48649cb21c9601f9be5fd424354e20b58af79
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-03 15:25:17 -0500

    problem 5 added

---
+++ problem5.js
@@ -0,0 +1,6 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "mewo" , roll: 96 ,department: "cse"  }
+
+;
+//write your code here
+console.log(student.name.concat(student.roll).concat(".").concat(student.department).concat("@ph.ac.bd"));
